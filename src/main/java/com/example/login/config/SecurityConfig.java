1d06ec57fe5c173a19cf289b6e186084232736ba
adriangmar89 <pcadrianpc@gmail.com>
2025-06-29 21:59:23 +0200

    Correcion warning

--- src/main/java/com/example/login/config/SecurityConfig.java
+++ src/main/java/com/example/login/config/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.example.login.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -16,6 +18,10 @@ public class SecurityConfig {
 
     @Value("${jwt.secret}") // Inyectar la clave secreta desde application.properties
     private String SECRET_KEY;
+
+    public SecurityConfig() {
+        // Constructor vacío, ya que no necesitamos ningún UserDetailsService.
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -38,6 +44,12 @@ public class SecurityConfig {
     @Bean
     public JwtAuthenticationFilter jwtAuthenticationFilter() {
         return new JwtAuthenticationFilter(SECRET_KEY); // Pasar la clave secreta al filtro
+    }
+
+    // El AuthenticationManager para eliminar el warning de "generated security password"
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
 }
